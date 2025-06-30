edbf678ea05e889c725165efd00efcbf97756d93
yashh-workout <jzhdhsu92@gmail.com>
2025-06-30 14:00:35 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("workout-cache").then(cache =>
+      cache.addAll(["index.html", "manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
